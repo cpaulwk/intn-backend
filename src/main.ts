3e6b3f1b9a2eaf463ad36de35fd2a0e2e39d6b74
Paul Chow <paulwk.chow@gmail.com>
2024-09-04 14:12:48 +0200

    feat(core): Configure main app and database connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  await app.listen(3001);
 }
 bootstrap();
